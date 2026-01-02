da75a9825ed0e7f128d6ed2e12cc0e7aa6465ee1
YusufBee1 <152460143+YusufBee1@users.noreply.github.com>
2026-01-02 16:59:17 -0500

    Fix: Force connection to Postgres Database

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -78,12 +78,18 @@ WSGI_APPLICATION = 'recipe_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# Force Heroku to use Postgres, but keep SQLite for local dev
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
